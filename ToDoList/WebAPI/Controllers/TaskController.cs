79a1fd7d2fe96d14e31ed1387256286762b2e983
Sue Burt <sookburt@gmail.com>
2020-10-15 17:41:05 +0100

    fixed controller to now use viewmodel rather than db model

--- ToDoList/WebAPI/Controllers/TaskController.cs
+++ ToDoList/WebAPI/Controllers/TaskController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DataAccess.Models;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.DataAccess;
+using WebAPI.ViewModels;
 
 namespace WebAPI.Controllers
 {
@@ -21,17 +18,17 @@ namespace WebAPI.Controllers
     
     
     [HttpGet] // GET api/task
-    public ActionResult<IEnumerable<ToDoItemDbModel>> Get()
+    public ActionResult<IEnumerable<ToDoItemViewModel>> Get()
     {
-      List<ToDoItemDbModel> tasks = _data.GetAllTasks();
+      List<ToDoItemViewModel> tasks = _data.GetAllTasks();
       return tasks;
     }
 
     
     [HttpGet("{id}")] // GET api/task/5
-    public ActionResult<ToDoItemDbModel> Get(int id)
+    public ActionResult<ToDoItemViewModel> Get(int id)
     {
-      ToDoItemDbModel item = _data.GetTaskById(id);
+      ToDoItemViewModel item = _data.GetTaskById(id);
 
       if(item == null)
       {
@@ -43,14 +40,14 @@ namespace WebAPI.Controllers
 
     
     [HttpPost] // POST api/task
-    public void Post([FromBody] ToDoItemDbModel item)
+    public void Post([FromBody] ToDoItemViewModel item)
     {
 
     }
 
     
     [HttpPut("{id}")] // PUT api/task/5
-    public void Put(int id, [FromBody] ToDoItemDbModel item)
+    public void Put(int id, [FromBody] ToDoItemViewModel item)
     {
     }
 
